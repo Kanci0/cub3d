f8942544dc90fa9b85f66ddae1558a10809b0a48
Kanci0 <162518477+Kanci0@users.noreply.github.com>
2025-04-02 18:57:51 +0200

    Add files via upload

--- cub3D/includes/cub3d.h
+++ cub3D/includes/cub3d.h
@@ -1,8 +1,7 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../minilibx/mlx.h"
-
+# include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -13,12 +12,37 @@
 # include <stdbool.h>
 # include <errno.h>
 # include <stdio.h>
+# include <math.h>
+
+# define WIN_WIDTH 1920
+# define WIN_HEIGHT 1080
+# define FOV 60 * (M_PI / 180)
+# define MOVE_SPEED 0.1
+# define ROTATE_SPEED 0.05
+# define NUM_RAYS WIN_WIDTH
+# define TILE_SIZE 1
+
+typedef struct s_img
+{
+    void    *img;
+    char    *addr;
+    int     bpp;
+    int     line_len;
+    int     endian;
+    int     width;  // Dodane pola dla szerokości tekstury
+    int     height; // i wysokości tekstury
+}   t_img;
+
 
 typedef struct{
-	char *NO;
-	char *SO;
-	char *WE;
-	char *EA;
+	t_img   NO;
+    t_img   SO;
+    t_img   WE;
+    t_img   EA;
+	char *no_path;
+	char *so_path;
+	char *we_path;
+	char *ea_path;
 	char *F;
 	char *C;
 	int *floor_color;
@@ -31,15 +55,20 @@ typedef struct {
 	int lines_tilmap;
 	int highest_x;
 	int highest_y;
+	int current_row;
 }	t_map;
 
 typedef struct {
 	int is_player;
-	int player_x;
-	int player_y;
+	float	px;
+	float	py;
+	float	pa;
 }	t_player;
 
 typedef struct{
+	void	*mlx;
+	void	*win;
+	t_img	img;
 	t_texture textures;
 	t_map map;
 	t_player player;
@@ -52,5 +81,6 @@ void 	check_x_and_y(t_game *game, char *str);
 void  	alloc_map(t_game *game, char *str);
 void 	color_convert(t_game *game);
 void 	map_check(t_game *game);
+bool 	is_player_value(char c);
 
 #endif
